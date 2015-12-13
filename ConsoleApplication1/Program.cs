34396844910668340139c20c11af97b9ebcb70e0
Oleksandr <stakhmich.oleksandr@gmail.com>
2015-12-14 00:00:55 +0200

    I added Hello world

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
         }
     }
 }
